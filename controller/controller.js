002f4868891e6486402b4f4d1a1dd2ac87f82edc
princeraj2003 <126746978+princeraj2003@users.noreply.github.com>
2024-10-08 12:30:40 +0530

    Api update

--- controller/controller.js
+++ controller/controller.js
@@ -13,9 +13,13 @@ async function fetchDataFromJobSearchAPI(req, res) {
       country: 'india',
       city: 'gurugram',
     },
-    headers: {
-      'X-RapidAPI-Key': 'ad3a8ebe22msh214895c63b0598ap13c572jsn601db0b8a9ca',
-    'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
+    // headers: {
+    //   'X-RapidAPI-Key': 'ad3a8ebe22msh214895c63b0598ap13c572jsn601db0b8a9ca',
+    // 'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
+    // },
+     headers: {
+      'x-rapidapi-key': '07c05c07d6msh4d8b6bfafd52f1fp1a6cbfjsn1765f358b68c',
+      'x-rapidapi-host': 'job-search-api1.p.rapidapi.com'
     },
   };
 
